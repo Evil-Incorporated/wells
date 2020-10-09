907e3ba19b8f7f3dae57b8345c12ecf3f242733b
Evil-Incorporated <rajarshi.banerjee47@gmail.com>
2020-10-10 03:27:27 +0530

    Added pre-processing

---
+++ vehicle_data.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+
+def get_data():
+    vehicle = pd.read_csv('Train.csv')
+    vehicle_travelling = pd.read_csv('Train_Vehicletravellingdata.csv')
+    vehicle_travelling['Date time'] = pd.to_datetime(vehicle_travelling['Date time'])
+    weather = pd.read_csv('Train_WeatherData.csv')
+    weather['Date time'] = pd.to_datetime(weather['Date time'])
+    data = vehicle.set_index('ID').join(vehicle_travelling.set_index('ID'), how='left').reset_index()
+    data = data.set_index(['ID', 'Date time']).join(weather.set_index(['ID', 'Date time']), how='outer').reset_index()
+    return data
+
+
+if __name__ == '__main__':
+    data = get_data()
