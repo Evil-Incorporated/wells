fc1cc8d47e991ee9125eafe7ebcb0b76cf2a8c56
Evil-Incorporated <rajarshi.banerjee47@gmail.com>
2020-10-12 23:11:27 +0530

    Fixed merging with the tables

--- vehicle_data.py
+++ vehicle_data.py
@@ -9,10 +9,14 @@ def get_data():
     weather = pd.read_csv('Train_WeatherData.csv')
     weather['Date time'] = pd.to_datetime(weather['Date time'])
     weather = pd.get_dummies(weather, columns=['Precipitation', 'Precipitation intensity', 'Day time'])
-    data = vehicle.set_index('ID').join(vehicle_travelling.set_index('ID'), how='left').reset_index()
-    data = data.set_index(['ID', 'Date time']).join(weather.set_index(['ID', 'Date time']), how='outer').reset_index()
+    weather = weather.drop(['ID', 'Date time'], axis=1)
+    data = vehicle_travelling.merge(weather, left_index=True, right_index=True)
+    print(data.columns)
+    data = vehicle.set_index('ID').join(data.set_index('ID'), how='left').reset_index()
+    # print(len(data.index))
+    # print(len(weather.index))
+    # data = data.set_index(['ID', 'Date time']).join(weather.set_index(['ID', 'Date time']), how='left').reset_index()
     return data
-
 
 if __name__ == '__main__':
     data = get_data()
